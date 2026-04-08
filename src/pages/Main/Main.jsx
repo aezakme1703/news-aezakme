6542b86ed8f026b20a874e1cb286bc4c63b1cbbf
aezakme <trapstar1703@gmail.com>
2026-04-08 12:37:05 +0300

    add search input and debounce hook

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from 'react'
+import { useDebounce } from '../../helpers/hooks/useDebounce'
 import NewsBanner from '../../components/NewsBanner/NewsBanner'
 import { getCategories, getNews } from '../../api/apiNews'
 import NewsList from '../../components/NewsLIst/NewsList'
 import Skeleton from '../../components/Skeleton/Skeleton'
 import Pagination from '../../components/Pagination/Pagination'
+import Search from '../../components/Search/Search'
+import Categories from '../../components/Categories/Categories'
 
 import styles from './styles.module.css'
-import Categories from '../../components/Categories/Categories'
+
 const Main = () => {
   const [ news, setNews ] = useState([])
   const [ isLoading, setIsLoading ] = useState(true)
   const [ currentPage, setCurrentPage ] = useState(1)
   const [ categories, setCategories ] = useState([])
+  const [ keywords, setKeywords] = useState('')
   const [ currentCategory, setCurrentCategory ] = useState('All')
   const totalPages = 10
   const pageSize = 10
+  const debouncedKeywords = useDebounce(keywords, 1500)
 
   // Функция получения новостей
   const fetchNews= async (currentPage) => {
@@ -23,7 +28,8 @@ const Main = () => {
       const response = await getNews({
         page_number: currentPage,
         page_size: pageSize,
-        category: currentCategory === 'All' ? null : currentCategory
+        category: currentCategory === 'All' ? null : currentCategory,
+        keywords: debouncedKeywords
       })
       setNews(response.news)
       setIsLoading(false)
@@ -49,7 +55,7 @@ const Main = () => {
 
   useEffect(() => {
     fetchNews(currentPage)
-  }, [currentPage, currentCategory])
+  }, [currentPage, currentCategory, debouncedKeywords])
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
@@ -64,10 +70,10 @@ const Main = () => {
   const handlePageClick = (pageNumber) => {
     setCurrentPage(pageNumber)
   }
-  
   return (
     <main className={styles.main}>
       <Categories categories={categories} currentCategory={currentCategory} setCurrentCategory={setCurrentCategory}/>
+      <Search keywords={keywords} setKeywords={setKeywords}/>
       {news.length > 0 && !isLoading ? (<NewsBanner item={news[0]}/>) : (<Skeleton type='banner' count={1}/>)}
       <Pagination currentPage={currentPage} handleNextPage={handleNextPage} handleBackPage={handleBackPage} handlePageClick={handlePageClick} totalPages={totalPages}/>
       {news.length > 0 && !isLoading ? (<NewsList news={news}/>) : (<Skeleton type='item' count={10}/>)}
